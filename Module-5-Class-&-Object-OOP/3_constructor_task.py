f8c819c8b8d920e50311e0716c9d1db8fdd2e3b2
marjanhasan <moonmarjanhasan@gmail.com>
2023-09-30 23:23:39 +0600

    constructor task added from moudle 3

---
+++ Module-5-Class-&-Object-OOP/3_constructor_task.py
@@ -0,0 +1,9 @@
+class Pen:
+    def __init__(self, name, price, color):
+        self.name = name
+        self.price = price
+        self.color = color
+
+
+my_pen = Pen("Metador", 10, "Purple")
+print(my_pen.name, my_pen.price, my_pen.color)  # Metador 10 Purple
