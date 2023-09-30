d38acece7af78f412fe39180b4b8707dff0957da
marjanhasan <moonmarjanhasan@gmail.com>
2023-09-30 22:24:01 +0600

    1-phone.py added to understand basic class syntax

---
+++ Module-5-Class-&-Object-OOP/1_phone_basic_class.py
@@ -0,0 +1,11 @@
+class Phone:
+    brand = "Samsung"
+    price = 19000
+    color = "Blue"
+
+
+my_phone = Phone()
+print(my_phone)  # <__main__.Phone object at 0x7fc1e9a5b5b0>
+print(my_phone.brand)  # Samsung
+print(my_phone.price)  # 19000
+print(my_phone.color)  # Blue
