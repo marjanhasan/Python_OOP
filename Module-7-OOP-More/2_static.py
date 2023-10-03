52f6cce732ad7fdbf4d3dc58e601de8e45a797ff
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-04 01:38:12 +0600

    Static and getter and setter added

---
+++ Module-7-OOP-More/2_static.py
@@ -0,0 +1,24 @@
+class Shopping:
+    cart = []  # class attribute or static attribute
+    origin = "Chine"
+
+    def __init__(self, name, location) -> None:
+        self.name = "Jamu na City"  # instance attribute
+        self.location = location  # jeta obj er sathe value change krte pare
+
+    def purchase(self, item, price, amount):
+        remaining = amount - price
+        print(f"Purchasing {item} for {price} and remaining {remaining}")
+
+    @staticmethod  # eta use krle instance charai use krbo
+    def multiply(a, b):
+        res = a * b  # etar vetore self.name evabe kaj krbe na
+        print(res)
+
+    @classmethod
+    def hudai(self, item):
+        print("hudai dekhbar aisi", item)
+
+
+Shopping.hudai("lungi")
+Shopping.multiply(4, 5)  # static method usage
---
+++ Module-7-OOP-More/3_getter_setter.py
@@ -0,0 +1,34 @@
+# read-only --> you can not set the value. Value can not be changed
+# getter --> get a value of a property through a method. Most of time, you will get the value of a private attribute
+# setter --> set a value of a property through a method. Most of time, you will set the value of a private property
+class User:
+    def __init__(self, name, age, money) -> None:
+        self.name = name
+        self._age = age
+        self.__money = money
+
+    # getter without setter
+    @property
+    def age(self):
+        return self._age
+
+    # getter with setter
+    @property
+    def salary(self):
+        return self.__money
+
+    # setter
+    @salary.setter
+    def salary(self, value):
+        if value < 0:
+            return f"Can not be negative"
+        else:
+            self.__money += value
+
+
+samsu = User("Kopa", 21, 12000)
+# the way to access getter and setter property
+print(samsu.age)
+print(samsu.salary)
+samsu.salary = 4500
+print(samsu.salary)
