6aead0f260fcd83401228dc4f86fa9b260eb93bb
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-11 19:30:13 +0600

    module 11 done

--- Module-11-School-Management/Persons.py
+++ Module-11-School-Management/Persons.py
@@ -1,4 +1,5 @@
 import random
+from School import School
 
 
 class Person:
@@ -7,27 +8,39 @@ class Person:
 
 
 class Teacher(Person):
-    def __init__(self, name, subject) -> None:
+    def __init__(self, name) -> None:
         super().__init__(name)
-        self.subject = subject
 
     def teach(self):
         pass
 
-    def take_exam(self, subjects, students):
-        for student in students:
-            marks = random.randint(0, 100)
-            # set marks for the subject for each student
+    def __repr__(self) -> str:
+        return f"{self.name}"
+
+    def evaluate_exam(self):
+        marks = random.randint(0, 100)
+        return marks
 
 
 class Student(Person):
-    def __init__(self, name) -> None:
+    def __init__(self, name, classroom) -> None:
         super().__init__(name)
-        self.classroom = None
+        self.classroom = classroom
         self.__id = None
-        self.subjects = []
         self.marks = {}
+        self.subject_grade = {}
         self.grade = None
+
+    def calculate_final_grade(self):
+        sum = 0
+        for grade in self.subject_grade.values():
+            point = School.grade_to_value(grade)
+            sum += point
+
+            print(self.name, grade, point)
+        points_avg = sum / len(self.subject_grade)
+        self.grade = School.value_to_grade(points_avg)
+        print(f"{self.name} final grade: {self.grade} with points avg {points_avg}")
 
     @property
     def id(self):
