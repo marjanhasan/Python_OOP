267fc98c6973bf40a853cfa0002f711451e96b8c
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-11 06:42:58 +0600

    Persons class added for teacher and student

--- Module-11-School-Management/School.py
+++ Module-11-School-Management/School.py
@@ -2,10 +2,14 @@ class School:
     def __init__(self, name, address) -> None:
         self.name = name
         self.address = address
+        self.teachers = {}
         self.classrooms = {}
 
     def add_classroom(self, classroom):
         self.classrooms[classroom.name] = classroom
+
+    def add_teacher(self, subject, teacher):
+        self.teachers[subject] = teacher
 
     def student_admission(self, student, classroom_name):
         if classroom_name in self.classrooms:
@@ -21,6 +25,8 @@ class ClassRoom:
 
     def add_student(self, student):
         serial_id = f"{self.name}-{len(self.students)+1}"
+        student.id = serial_id
+        student.classroom = self.name
         self.students.append(student)
 
     def __str__(self) -> str:
